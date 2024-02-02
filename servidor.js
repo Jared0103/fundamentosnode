d57d5008bf77550893442a79a338e0c713cc6bc8
Jared Ochoa <josejared8ac@hotmail.com>
2024-02-01 21:11:34 -0600

    Instalamos firebase

--- servidor.js
+++ servidor.js
@@ -1,6 +1,21 @@
 const express = require('express')
+import { initializeApp } from "firebase/app";
 const app = express()
 const port = 5000
+
+//conexion a la base de datos en Firebaase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNzJrKou5ZQV6DXnJd09hCU6hBAiCkhok",
+  authDomain: "crud-practica1-6707b.firebaseapp.com",
+  projectId: "crud-practica1-6707b",
+  storageBucket: "crud-practica1-6707b.appspot.com",
+  messagingSenderId: "260068706990",
+  appId: "1:260068706990:web:61df83d911d4e2ffd07b3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 app.get('/', (req, res) =>{
     res.send('Respuesta de raiz')
